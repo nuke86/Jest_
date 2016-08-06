399d6a3ab06355430b111db3a0269c30b687b4be
root <nukeadmi@tiscali.it>
2016-08-06 16:59:20 +0200

    new github link

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
       <div class="container" style="margin-left: 10px;">
-           <p class="text-muted credit"> <a href="http://www.mercusa.it" target="labs">Gest_</a> is Powered by <a href="http://www.spcnet.it/labs" target="labs">Spcnet - Labs</a></p>
+           <p class="text-muted credit"> <a href="https://github.com/nuke86/Jest" target="gh">Jest_</a> on GitHub is Powered by <a href="http://www.spcnet.it/labs" target="labs">Spcnet - Labs</a></p>
 
       </div>
 </footer>
