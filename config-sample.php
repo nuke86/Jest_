21042c730da3145e25f67bbb0ae8d05c573c59fe
root <nukeadmi@tiscali.it>
2016-08-06 16:23:31 +0200

    correzione configurazione

---
+++ config-sample.php
@@ -0,0 +1,21 @@
+<?php
+// configurazione del programma
+//ini_set('session.gc_maxlifetime','14400');  // impostazione per la durata delle sessioni.
+
+// Dati e Intestazioni aziendali
+$nome_azienda = "ASD JUDO";
+$costo_mese_config = "35.00"; // il costo fisso del mensile per ogni atleta
+
+// impostazioni del database Mysql
+$mysql_ip = "localhost";
+$db_name = "";
+$db_user = "root";
+$db_pass = "";
+
+// connessione utilizzata per tutte le funzioni di Database in tutto MercUsa
+$con=mysqli_connect($mysql_ip,$db_user,$db_pass,$db_name);
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
