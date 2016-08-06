981add6265981902c3e0e4e865f06491f683b55b
root <nukeadmi@tiscali.it>
2016-08-06 16:20:54 +0200

    correzione configurazione

--- config.php
+++ config.php
@@ -3,14 +3,14 @@
 //ini_set('session.gc_maxlifetime','14400');  // impostazione per la durata delle sessioni.
 
 // Dati e Intestazioni aziendali
-$nome_azienda = "FoxClub Judo";
+$nome_azienda = "ASD JUDO";
 $costo_mese_config = "35.00"; // il costo fisso del mensile per ogni atleta
 
 // impostazioni del database Mysql
 $mysql_ip = "localhost";
-$db_name = "gest_foxclub";
+$db_name = "";
 $db_user = "root";
-$db_pass = "70r853eka2";
+$db_pass = "";
 
 // connessione utilizzata per tutte le funzioni di Database in tutto MercUsa
 $con=mysqli_connect($mysql_ip,$db_user,$db_pass,$db_name);
